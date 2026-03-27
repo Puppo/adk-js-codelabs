4f20255d04aab22a36d99e80e6eb1375ebc812a7
Luca Del Puppo <Puppo@users.noreply.github.com>
2026-03-27 19:38:14 +0100

    refactor: simplify data import by using JSON modules instead of file system reads

--- src/common/conferenceData.ts
+++ src/common/conferenceData.ts
@@ -1,20 +1,13 @@
-import { readFileSync } from "node:fs";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
 import { conferenceSchema } from "./conferenceSchema.js";
 import { scheduleSchema } from "./scheduleSchema.js";
 import { speakersSchema } from "./speakersSchema.js";
 
-const dataDir = resolve(dirname(fileURLToPath(import.meta.url)), "../../data");
+import conferenceJSON from "../../data/conference.json" with { type: "json" };
+import scheduleJSON from "../../data/schedule.json" with { type: "json" };
+import speakersJSON from "../../data/speakers.json" with { type: "json" };
 
-export const conference = conferenceSchema.parse(
-  JSON.parse(readFileSync(resolve(dataDir, "conference.json"), "utf-8")),
-);
+export const conference = conferenceSchema.parse(conferenceJSON);
 
-export const speakers = speakersSchema.parse(
-  JSON.parse(readFileSync(resolve(dataDir, "speakers.json"), "utf-8")),
-);
+export const speakers = speakersSchema.parse(speakersJSON);
 
-export const schedule = scheduleSchema.parse(
-  JSON.parse(readFileSync(resolve(dataDir, "schedule.json"), "utf-8")),
-);
+export const schedule = scheduleSchema.parse(scheduleJSON);
